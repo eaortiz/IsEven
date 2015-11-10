e344cab559e091014be206595728df9499e7d04d
Estefania Ortiz <eaortiz@stanford.edu>
2015-11-10 13:03:18 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-
         System.out.println(isEven("lineline"));
         System.out.println(isEven("lineine"));
 
         System.out.println(isEven("lol"));
         System.out.println(isEven("lolo"));
 
-	// write your code here
     }
 
     private static boolean isEven(String word) {
@@ -20,7 +17,7 @@ public class Main {
             return true;
         }
 
-        char c = word.charAt(0);
+        char c = word.charAt(0); //bad variable name
         word = word.substring(1);
 
         if(word.contains(c + ""))
@@ -31,5 +28,6 @@ public class Main {
 
         return false;
     }
+    //this is pretty much the same as Ray, try to do it by yourself you wont be teaming up to interview
 
 }
